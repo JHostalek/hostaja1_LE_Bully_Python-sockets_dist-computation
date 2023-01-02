0420ef328106c5e15f29e6d7373c476aac4f2977
JHostalek <jan.hostalek@post.cz>
2023-01-02 17:03:27 +0100

    broadcast fix

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
 import socket
 import struct
-import sys
 import time
 
-if __name__ == '__main__':
-    message = time.ctime().encode('ascii')
+# Create the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Create the socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Set the IP and port to listen on
+bind_address = ('192.168.56.1', 10000)
 
-    # Set a timeout so the socket does not block indefinitely when trying
-    # to receive data.
-    sock.settimeout(1)
+# Bind to the specific IP address and port
+sock.bind(bind_address)
 
-    # Set the time-to-live for messages to 1 so they do not go past the
-    # local network segment.
-    ttl = struct.pack('b', 10)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+# Set timeout for socket to 1 second
+sock.settimeout(1)
 
-    # Bind to the server address
-    server_address = ('', 5555)
-    sock.bind(server_address)
+# Tell the operating system to add the socket to the multicast group
+# on all interfaces.
+multicast_group = ('224.3.29.71', 10000)
+group = socket.inet_aton(multicast_group[0])
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-    multicast_group = ('224.3.29.71', 5555)
-
-    # Tell the operating system to add the socket to the multicast group
-    # on all interfaces.
-    group = socket.inet_aton('224.3.29.71')
-    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
-    # Receive/respond loop
+# Receive messages
+try:
     while True:
-        try:
-            print('\nWaiting to receive message')
-            data, address = sock.recvfrom(1024)
+        print('Listening for messages...')
+        data, address = sock.recvfrom(1024)
+        print(f'Received message from {address}: {data}')
 
-            print(f'Received {len(data)} bytes from {address}')
-            print(data)
-
-            # print(f'Sending acknowledgement to {address}')
-            # sock.sendto(b'ACK', address)
-        except socket.timeout:
-            print('Timed out, trying again')
-            sock.sendto(message, multicast_group)
-            continue
-        except KeyboardInterrupt:
-            print('Exiting program')
-            break
-
-    # Leave the multicast group
+        # Send a message to the multicast group every 5 seconds
+        time.sleep(5)
+        message = f'Keepalive from {bind_address}'
+        sock.sendto(message, multicast_group)
+except KeyboardInterrupt:
+    print('Exiting...')
+finally:
+    # Leave the multicast group and close the socket
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
     sock.close()
