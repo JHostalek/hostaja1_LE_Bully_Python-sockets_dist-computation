0b19a5da361fa1c392e383cf008ee00eb5b7b9fc
JHostalek <jan.hostalek@post.cz>
2023-01-02 17:27:10 +0100

    broadcast perms

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
+ip_address = socket.gethostbyname(socket.gethostname())
 
 # Set the IP and port to listen on
 bind_address = ('192.168.56.255', 10000)
@@ -20,7 +20,8 @@ try:
     while True:
         print('Listening for messages...')
         data, address = sock.recvfrom(1024)
-        print(f'Received message from {address}: {data}')
+        print(f'Received message from {address} {ip_address}: {data}')
+        if address == ip_address: continue
         time.sleep(5)
         sock.sendto(b'Keepalive', ('192.168.56.255', 10000))
 except KeyboardInterrupt:
