00043c8316f6996310607bd7de80da6d5c629a8b
JHostalek <jan.hostalek@post.cz>
2023-01-02 16:55:54 +0100

    broadcast ttl

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 if __name__ == '__main__':
-    message = time.ctime().encode('ascii')
+    message = socket.gethostname().encode('ascii')
 
     # Create the socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     # Set the time-to-live for messages to 1 so they do not go past the
     # local network segment.
-    ttl = struct.pack('b', 1)
+    ttl = struct.pack('b', 10)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
     # Bind to the server address
