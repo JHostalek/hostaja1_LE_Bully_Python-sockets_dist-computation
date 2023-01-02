c3dd6c1bd396498cd49517bfcf397f07ba4046ca
JHostalek <jan.hostalek@post.cz>
2023-01-02 20:52:42 +0100

    broadcastSock fix

--- NetworkUtils.py
+++ NetworkUtils.py
@@ -8,24 +8,24 @@ class NetworkUtils:
 
         self.ip = self.parseIp()
         self.BROADCAST_PORT = 5555
-        self.broadcastSock = self.initBroadcast()
+        self.broadcastSock = None
+        self.initBroadcast()
 
     def initBroadcast(self):
         # Create the socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 1)
+        self.broadcastSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.broadcastSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.broadcastSock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 1)
 
         # Set the IP and port to listen on
         # TODO: ALLOW LOCALHOST
         bind_address = ('192.168.56.255', self.BROADCAST_PORT)
 
         # Bind to the specific IP address and port
-        sock.bind(bind_address)
+        self.broadcastSock.bind(bind_address)
         # Create threads for sending and receiving messages
         receive_thread = threading.Thread(target=self.listenBroadcast)
         receive_thread.start()
-        return sock
 
     def listenBroadcast(self):
         while not self.terminate.is_set():
