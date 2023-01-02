23d8ee11a11670e87569b748930bbc3087b0def4
JHostalek <jan.hostalek@post.cz>
2023-01-02 16:50:08 +0100

    broadcast rollback

--- main.py
+++ main.py
@@ -2,37 +2,48 @@ import socket
 import struct
 import sys
 
-message = 'Hello, world!'
-multicast_group = ('224.3.29.71', 5555)
+if __name__ == '__main__':
 
-# Create the datagram socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Create the socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Set a timeout so the socket does not block indefinitely when trying
-# to receive data.
-sock.settimeout(1)
+    # Set a timeout so the socket does not block indefinitely when trying
+    # to receive data.
+    sock.settimeout(1)
 
-# Set the time-to-live for messages to 1 so they do not go past the
-# local network segment.
-ttl = struct.pack('b', 2)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    # Set the time-to-live for messages to 1 so they do not go past the
+    # local network segment.
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-try:
-    # Send data to the multicast group
-    print(f'sending {message}')
-    sent = sock.sendto(message.encode(), multicast_group)
-    # Look for responses from all recipients
+    # Bind to the server address
+    server_address = ('', 10000)
+    sock.bind(server_address)
+
+    # Tell the operating system to add the socket to the multicast group
+    # on all interfaces.
+    group = socket.inet_aton('224.3.29.71')
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    # Receive/respond loop
     while True:
-        print('waiting to receive')
         try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            pass
-        except KeyboardInterrupt:
-            break
-        else:
-            print(f'received {data} from {server}')
+            print('\nWaiting to receive message')
+            data, address = sock.recvfrom(1024)
 
-finally:
-    print('closing socket')
+            print(f'Received {len(data)} bytes from {address}')
+            print(data)
+
+            print(f'Sending acknowledgement to {address}')
+            sock.sendto(b'ACK', address)
+        except socket.timeout:
+            print('Timed out, trying again')
+            continue
+        except KeyboardInterrupt:
+            print('Exiting program')
+            break
+
+    # Leave the multicast group
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
     sock.close()
