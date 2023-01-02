f8243c056725a19dd436bb42d9a0bd4d41a9419e
JHostalek <jan.hostalek@post.cz>
2023-01-02 15:44:29 +0100

    multicast init

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import socket
+import struct
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Create the socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Set a timeout so the socket does not block indefinitely when trying
+    # to receive data.
+    sock.settimeout(0.2)
+
+    # Set the time-to-live for messages to 1 so they do not go past the
+    # local network segment.
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    # Bind to the server address
+    server_address = ('', 10000)
+    sock.bind(server_address)
+
+    # Tell the operating system to add the socket to the multicast group
+    # on all interfaces.
+    group = socket.inet_aton('224.3.29.71')
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    # Receive/respond loop
+    while True:
+        try:
+            print('\nWaiting to receive message')
+            data, address = sock.recvfrom(1024)
+
+            print(f'Received {len(data)} bytes from {address}')
+            print(data)
+
+            print(f'Sending acknowledgement to {address}')
+            sock.sendto(b'ACK', address)
+        except socket.timeout:
+            print('Timed out, trying again')
+            continue
+        except KeyboardInterrupt:
+            print('Exiting program')
+            break
+
+    # Leave the multicast group
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+    sock.close()
