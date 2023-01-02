0d2f886a889205a3263ef4fd17f4e2c21276dd34
JHostalek <jan.hostalek@post.cz>
2023-01-02 17:43:02 +0100

    broadcast cleanup init

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import time
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 1)
 
-import re
-import subprocess
 
-output = subprocess.run(["ip", "a"], stdout=subprocess.PIPE).stdout.decode()
+def get_IP():
+    import re
+    import subprocess
+    output = subprocess.run(["ip", "a"], stdout=subprocess.PIPE).stdout.decode()
+    address_pattern = r"inet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+    matches = re.findall(address_pattern, output)
+    return matches[2]
 
-address_pattern = r"inet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
 
-matches = re.findall(address_pattern, output)
-
-ip_address = matches[2]
-
+ip_address = get_IP()
+PORT = 5555
 # Set the IP and port to listen on
-bind_address = ('192.168.56.255', 10000)
+bind_address = ('192.168.56.255', PORT)
 
 # Bind to the specific IP address and port
 sock.bind(bind_address)
 
 # Set timeout for socket to 1 second
 sock.settimeout(20)
-sock.sendto(b'Keepalive', ('192.168.56.255', 10000))
+sock.sendto(b'Keepalive', ('192.168.56.255', PORT))
 try:
     while True:
         print('Listening for messages...')
@@ -34,7 +36,7 @@ try:
         print(f'Received message from {address} {ip_address}: {data}')
 
         time.sleep(1)
-        sock.sendto(b'Keepalive', ('192.168.56.255', 10000))
+        sock.sendto(b'Keepalive', ('192.168.56.255', PORT))
 except KeyboardInterrupt:
     print('Exiting...')
 finally:
