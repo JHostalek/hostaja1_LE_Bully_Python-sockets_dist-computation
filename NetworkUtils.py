7967cb8ecc23690575c6383300d7a7c6e87bcb5a
JHostalek <jan.hostalek@post.cz>
2023-01-03 11:48:04 +0100

    tcp rework fix

--- NetworkUtils.py
+++ NetworkUtils.py
@@ -52,10 +52,11 @@ class NetworkUtils:
         while not self.terminate.is_set():
             data, address = self.broadcastSock.recvfrom(1024)
             address = Address(address)
+            message = pickle.loads(data)
+
             print(message)
 
             if address != self.broadcastAddress:
-                message = pickle.loads(data)
                 if isinstance(message, RequestingConnection):
                     print(f"{self.broadcastAddress} - Received connection request from {address}")
                     self.processConnectionRequest(address)
