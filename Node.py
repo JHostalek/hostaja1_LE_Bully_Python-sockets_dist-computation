caa08045b3796980df1fa2bfef6973f49dfaf598
JHostalek <jan.hostalek@post.cz>
2023-01-03 08:56:22 +0100

    formatting

--- Node.py
+++ Node.py
@@ -10,5 +10,5 @@ class Node:
         self.nu.sendBroadcast(NotifyAllMessage(self).toBytes())
 
     def processBroadcast(self, message: NotifyAllMessage, sender: Address):
-        print(f"{self.nu.ip} - Received message from {sender} : {message.message} at logical time {message.clock} - {message.timestamp}")
+        print(f"{self.nu.broadcastAddress} - Received message from {sender} : {message.message} at logical time {message.clock} - {message.timestamp}")
         self.clock = max(self.clock, message.clock) + 1
