975a3309107a8ec25780d44c7e6c0c2d2c494189
JHostalek <jan.hostalek@post.cz>
2023-01-06 18:17:21 +0100

    parsing arguments

--- MainNode.py
+++ MainNode.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--real_audio', required=False, type=bool, default=False, help='Use real audio')
     parser.add_argument('--tasks', required=False, help='How many tasks to generate', type=int, default=20)
-    parser.add_argument('--data_center_ip', required=True, help='IP of the data center', type=str, default='192.168.56.115')
+    parser.add_argument('--data_center_ip', required=False, help='IP of the data center', type=str, default='192.168.56.115')
     parser.add_argument('--message_delay', required=False, help='Delay between all messages', type=float, default=1)
     parser.add_argument('--hold_each_message', required=False, help='Hold on to each message until key is pressed. Cancel by CTRL+E', type=bool, default=False)
     parser.add_argument('--bully_timeout', required=False,
