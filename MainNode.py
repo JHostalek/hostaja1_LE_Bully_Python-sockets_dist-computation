fdda8b9f572de37901ddd3e113e6f0ab036de0a5
JHostalek <jan.hostalek@post.cz>
2023-01-08 10:20:07 +0100

    fix

--- MainNode.py
+++ MainNode.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     parser.add_argument('--real_audio', required=False, type=bool, default=False, help='Use real audio')
     parser.add_argument('--tasks', required=False, help='How many tasks to generate', type=int, default=20)
     parser.add_argument('--data_center_ip', required=False, help='IP of the data center', type=str, default='192.168.56.115')
-    parser.add_argument('--message_delay', required=False, help='Delay between all messages', type=float, default=1)
+    parser.add_argument('--message_delay', required=False, help='Delay between all messages', type=float, default=0)
     parser.add_argument('--hold_each_message', required=False, help='Hold on to each message until key is pressed. Cancel by CTRL+E', type=bool, default=False)
     parser.add_argument('--bully_timeout', required=False,
                         help='Time limit for the bully algorithm. Node claims to be the coordinator (leader) if neighbors with higher id do not respond until this timeout expires.',
